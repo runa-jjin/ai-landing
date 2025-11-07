7264d78bd79d1399a31c5b08f934bb54cc251be6
jjin <tears0427@gmail.com>
2025-11-07 23:57:57 +0900

    Fix AdSense data-nscript attribute issue by dynamically injecting script client-side

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: {
@@ -5,13 +6,35 @@ const nextConfig = {
     },
   },
   // Windows 파일 잠금 문제 해결
-  webpack: (config, { dev }) => {
+  webpack: (config, { dev, isServer }) => {
     if (dev) {
       config.watchOptions = {
         poll: 1000,
         aggregateTimeout: 300,
       };
     }
+    
+    // HTML에서 data-nscript 속성 제거를 위한 플러그인
+    if (!isServer) {
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.emit.tap('RemoveDataNscript', (compilation) => {
+            Object.keys(compilation.assets).forEach((filename) => {
+              if (filename.endsWith('.html')) {
+                let source = compilation.assets[filename].source();
+                source = source.replace(/data-nscript="[^"]*"/g, '');
+                source = source.replace(/data-nscript='[^']*'/g, '');
+                compilation.assets[filename] = {
+                  source: () => source,
+                  size: () => source.length,
+                };
+              }
+            });
+          });
+        },
+      });
+    }
+    
     return config;
   },
 };
